57d8a3388ee8368f1d98647767b0306a9e2f3a5a
Tibi <110664232+TiberiuGC@users.noreply.github.com>
2024-08-27 11:08:37 +0300

    Add support for RuleGroup CRD (#6)
    Issue #, if available: https://github.com/aws-controllers-k8s/community/issues/1300
    
    Description of changes:
    - on top of, and to be reviewed after merging: https://github.com/aws-controllers-k8s/wafv2-controller/pull/4
    - adds support for RuleGroup CRD
    - adds integration tests for above
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

---
+++ test/e2e/rule_group.py
@@ -0,0 +1,60 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# 	 http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+"""Utilities for working with RuleGroup resources"""
+
+import datetime
+import time
+
+import boto3
+import pytest
+
+DELETE_WAIT_TIME_TIMEOUT_SECONDS = 60 * 10
+
+
+def wait_until_deleted(
+    name: str,
+    id: str,
+    timeout_seconds: int = DELETE_WAIT_TIME_TIMEOUT_SECONDS,
+    interval_seconds: int = 15,
+) -> None:
+    """Waits until a RuleGroup with a supplied Name and ID is no longer returned from
+    the WAFv2 API.
+
+    Raises:
+        pytest.fail upon timeout
+    """
+    now = datetime.datetime.now()
+    timeout = now + datetime.timedelta(seconds=timeout_seconds)
+
+    while True:
+        if datetime.datetime.now() >= timeout:
+            pytest.fail("Timed out waiting for RuleGroup to be " "deleted in WAFv2 API")
+        time.sleep(interval_seconds)
+
+        latest = get(name, id)
+        if latest is None:
+            break
+
+
+def get(
+    name: str,
+    id: str,
+    scope: str = "REGIONAL",
+):
+    c = boto3.client("wafv2")
+    try:
+        resp = c.get_rule_group(Name=name, Id=id, Scope=scope)
+        return resp["RuleGroup"]
+    except c.exceptions.WAFNonexistentItemException:
+        return None
